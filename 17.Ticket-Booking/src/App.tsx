56fc2c335a003c4c490800792e38a695ce6abfa6
Deepak Kumar <bop@festgroup.in>
2025-12-07 18:57:29 +0530

    Ticket booking

---
+++ 17.Ticket-Booking/src/App.tsx
@@ -0,0 +1,10 @@
+import ScreenComponents from "./components/ScreenComponents";
+
+function App() {
+  return (
+    <ScreenComponents/>
+
+  );
+}
+
+export default App;
---
+++ 17.Ticket-Booking/src/components/ScreenComponents.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ScreenComponents = () => {
+  return (
+    <div className='min-h-screen w-full flex items-center justify-center p-4 md:p-6 lg:p-8'>
+        {/*  */}
+
+    </div>
+  )
+}
+
+export default ScreenComponents
